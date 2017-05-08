e4b3bd6205a1a153122922e100bb1476e5abe917
Sean Ross <seanmross9@gmail.com>
2017-05-07 20:53:56 -0400

    Updates to chat window: display time

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,6 +3,19 @@
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
 
+    function checkTime() {
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var period = 'am';
+      if (hours > 12) {
+        hours -= 12;
+        period = 'pm';
+      }
+      var time = hours + ":" + minutes + period;
+      return time;
+    }
+
     Message.getByRoomId = function(id) {
       //Logic for filtering messages
       messages = $firebaseArray(ref.orderByChild("roomId").equalTo(id));
@@ -16,10 +29,12 @@
       //set the roomId equal to roomId argument
       //use cookies service to set username
       //use .$add method to add message object to firebase [bloc-chat-f6e23 -> messages]
+      //var date = new Date();
+
       var message = {
         content: newMessage,
         roomId: id,
-        sentAt: '12:00',
+        sentAt: checkTime(),
         username: $cookies.get('blocChatCurrentUser'),
       };
 
